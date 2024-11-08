4965ff0599e4def605722184f0a1b2fb90544142
curso00 <curso00@nomail.com>
2024-11-08 14:00:58 +0100

    Métodos de suma y media, faltan test

--- src/main/java/es/santander/ascender/proyecto04/Arreglo.java
+++ src/main/java/es/santander/ascender/proyecto04/Arreglo.java
@@ -34,4 +34,32 @@ public class Arreglo {
 
         return destino;
     }
+
+    public float devolverValorEnIndice(float[] lista, int indiceABuscar) throws Exception  {
+        if (indiceABuscar < 0) {
+            throw new Exception("Me has pedido un índice negativo: " + indiceABuscar);
+        } else {
+            if (indiceABuscar >= lista.length) {
+                throw new Exception("Me has pedido un índice mayor que los permitidos: " + indiceABuscar);
+            }
+        }
+
+        return lista[indiceABuscar];
+    }
+
+    public double sumarValores(double[] valores) {
+        double suma = 0.0;
+
+        for (int i = 0; i < valores.length; i++) {
+            suma = suma + valores[i];
+        }
+
+        return suma;
+    }
+
+    public double calcularMediaValores(double[] valores) {
+        double suma = sumarValores(valores);
+
+        return suma/valores.length;
+    }
 }
--- src/test/java/es/santander/ascender/proyecto04/ArregloTest.java
+++ src/test/java/es/santander/ascender/proyecto04/ArregloTest.java
@@ -20,8 +20,6 @@ public class ArregloTest {
         for (int i = 0 ; i < TAMANO_REQUERIDO; i++) {
             assertEquals(i, resultado[i]);
         }
-
-        
     }
 
     @Test
@@ -32,5 +30,18 @@ public class ArregloTest {
         
         int[] resultadoDeLaPrueba = cut.crearInvirtiendoOrden(origenParaLaPrueba);
         assertArrayEquals(new int[]{7, 0, -1, 6, 3}, resultadoDeLaPrueba);
+    }
+
+
+    @Test
+    public void testDevolverValorEnIndice() throws Exception {
+        Arreglo cut = new Arreglo();
+
+        
+
+        float resultado = cut.devolverValorEnIndice(new float[] {4.6f, 3.2F, -34.7f, 0.0f}, 1);
+
+        assertEquals(3.2f, resultado);
+
     }
 }
