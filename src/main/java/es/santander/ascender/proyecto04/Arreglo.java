b46e03175245d11ec1a0ccbeb0d0e9ed46fc0040
curso00 <curso00@nomail.com>
2024-11-08 10:52:06 +0100

    Mejoras en el test y jugamos con static final y constantes

--- src/main/java/es/santander/ascender/proyecto04/Arreglo.java
+++ src/main/java/es/santander/ascender/proyecto04/Arreglo.java
@@ -1,9 +1,11 @@
 package es.santander.ascender.proyecto04;
 
 public class Arreglo {
+    private static final int TAMANO = 100;
+
     public int[] crearCienPrimeros() {
-        int[] resultado = new int[100];
-        for(int i = 0 ; i < 100; i = i + 1) {
+        int[] resultado = new int[TAMANO];
+        for(int i = 0 ; i < TAMANO; i = i + 1) {
             resultado[i] = i;
         }
         return resultado;
--- src/test/java/es/santander/ascender/proyecto04/ArregloTest.java
+++ src/test/java/es/santander/ascender/proyecto04/ArregloTest.java
@@ -6,15 +6,17 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import org.junit.jupiter.api.Test;
 
 public class ArregloTest {
+    private static final int TAMANO_REQUERIDO = 100;
+
     @Test
     public void testCrearCienPrimeros() {
         Arreglo arreglo = new Arreglo();
 
         int[] resultado = arreglo.crearCienPrimeros();
 
-        assertNotNull(resultado);
-        assertEquals(100, resultado.length);
-        for (int i = 0 ; i < 100; i++) {
+        assertNotNull(resultado, "El array recibido de la creación no debe ser nulo");
+        assertEquals(TAMANO_REQUERIDO, resultado.length, "Esperaba que arreglo fuera de tamaño 100");
+        for (int i = 0 ; i < TAMANO_REQUERIDO; i++) {
             assertEquals(i, resultado[i]);
         }
 
